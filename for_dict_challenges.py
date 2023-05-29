a90923a22596067bf6208acd4f4cbc71b7a856c3
Maksim Smirnov <ozonasnuf@gmail.com>
2023-05-29 21:22:07 +0300

    completed for_dict_challenges

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -1,3 +1,4 @@
+from collections import Counter
 # Задание 1
 # Дан список учеников, нужно посчитать количество повторений каждого имени ученика
 # Пример вывода:
@@ -12,7 +13,12 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Петя'},
 ]
-# ???
+
+print('Задание 1')
+qty_students = (Counter(student['first_name'] for student in students))
+for student in qty_students:
+    print(f'{student}: {qty_students[student]}')
+print()
 
 
 # Задание 2
@@ -26,7 +32,12 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Оля'},
 ]
-# ???
+
+print('Задание 2')
+most_common_student = Counter(student['first_name']
+                              for student in students).most_common(1)
+print(
+    f'Самое частое имя среди учеников: {most_common_student[0][0]}\n')
 
 
 # Задание 3
@@ -44,26 +55,35 @@ school_students = [
         {'first_name': 'Маша'},
         {'first_name': 'Маша'},
         {'first_name': 'Оля'},
-    ],[  # это – третий класс
+    ], [  # это – третий класс
         {'first_name': 'Женя'},
         {'first_name': 'Петя'},
         {'first_name': 'Женя'},
         {'first_name': 'Саша'},
     ],
 ]
-# ???
 
+print('Задание 3')
+for grade in range(len(school_students)):
+    max_students = Counter(student['first_name']
+                           for student in school_students[grade]).most_common(1)
+    print(
+        f'Самое частое имя в классе {grade + 1}: {max_students[0][0]}')
+print()
 
 # Задание 4
 # Для каждого класса нужно вывести количество девочек и мальчиков в нём.
 # Пример вывода:
-# Класс 2a: девочки 2, мальчики 0 
+# Класс 2a: девочки 2, мальчики 0
 # Класс 2б: девочки 0, мальчики 2
 
 school = [
-    {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
-    {'class': '2б', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
-    {'class': '2б', 'students': [{'first_name': 'Даша'}, {'first_name': 'Олег'}, {'first_name': 'Маша'}]},
+    {'class': '2a', 'students': [
+        {'first_name': 'Маша'}, {'first_name': 'Оля'}]},
+    {'class': '2б', 'students': [
+        {'first_name': 'Олег'}, {'first_name': 'Миша'}]},
+    {'class': '2б', 'students': [{'first_name': 'Даша'}, {
+        'first_name': 'Олег'}, {'first_name': 'Маша'}]},
 ]
 is_male = {
     'Олег': True,
@@ -72,9 +92,18 @@ is_male = {
     'Миша': True,
     'Даша': False,
 }
-# ???
-
-
+print('Задание 4')
+for grade in school:
+    male = 0
+    female = 0
+    for student in grade['students']:
+        if is_male[student['first_name']]:
+            male += 1
+        else:
+            female += 1
+    print(
+        f"Класс {grade['class']}: девочки {female}, мальчики {male}")
+print()
 # Задание 5
 # По информации о учениках разных классов нужно найти класс, в котором больше всего девочек и больше всего мальчиков
 # Пример вывода:
@@ -82,8 +111,10 @@ is_male = {
 # Больше всего девочек в классе 2a
 
 school = [
-    {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
-    {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
+    {'class': '2a', 'students': [
+        {'first_name': 'Маша'}, {'first_name': 'Оля'}]},
+    {'class': '3c', 'students': [
+        {'first_name': 'Олег'}, {'first_name': 'Миша'}]},
 ]
 is_male = {
     'Маша': False,
@@ -91,5 +122,21 @@ is_male = {
     'Олег': True,
     'Миша': True,
 }
-# ???
+print('Задание 5')
+male = []
+female = []
+for grade in range(len(school)):
+    male.append(0)
+    female.append(0)
+    for student in school[grade]['students']:
+        if is_male[student['first_name']]:
+            male[grade] += 1
+        else:
+            female[grade] += 1
 
+most_male = school[male.index(max(male))]['class']
+most_female = school[female.index(max(female))]['class']
+print(
+    f"Больше всего мальчиков в классе {most_male}")
+print(
+    f"Больше всего девочек в классе {most_female}")
